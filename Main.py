52d95a5aa984559c83085bf6ed6d1796e473130f
Jacob I. Komissar <jirassimok@gmail.comu>
2017-09-01 16:32:16 -0400

    Update Graph class, rename various functions

--- Main.py
+++ Main.py
@@ -2,25 +2,91 @@
 Project: SearchAlgorithms
 File: Main
 Created on: 8/28/2017
-  
+
 Project Description:
 CS4341 - Project 1
 Search algorithms
 
 A project in which several search algorithms are implemented.
 """
+from abc import ABC, abstractmethod, ABCMeta
 from collections import OrderedDict
-from functools import update_wrapper, wraps
+from functools import reduce
+from itertools import starmap
 import re
-from typing import NamedTuple, List, Dict, FrozenSet, Callable, Iterable, MutableMapping
+from typing import NamedTuple, List, Dict, Set, FrozenSet, Callable, Iterable, MutableMapping, Union, Tuple
 import sys
 
-class Graph:
+Path = Union[Tuple[str], List[str]]
+
+class GraphABC(ABC):
+    @abstractmethod
+    def heuristic(node: str) -> float:
+        """Get the heuristic value for a node, or 0 if the node has no heuristic value.
+        """
+    @abstractmethod
+    def neighbors(node: str) -> Set[str]:
+        """Get a safe-to-modify set of e the neighbors of the given node.
+        """
+    @abstractmethod
+    def distance(a: str, b: str) -> float:
+        """Get the distance between two nodes.
+
+        If the nodes are not connected, KeyError.
+        """
+    @abstractmethod
+    def path_cost(path: Path) -> float:
+        """Get the total distance of the edges connecting the listed nodes.
+
+        If any two adjacent nodes in the path are not connected, KeyError.
+        """
+
+class Graph(GraphABC):
     def __init__(self,
-                 edges: Dict[FrozenSet[str], float],
-                 heuristics: Dict[FrozenSet[str], float]):
-        self.edges = edges
-        self.heuristics = heuristics
+                 edge_lengths: Dict[FrozenSet[str], float],
+                 heuristics: Dict[str, float]):
+        self._edges = frozenset(edge_lengths.items())
+        self._heuristics = frozenset(heuristics.items())
+    @property
+    def edge_lengths(self) -> Dict[FrozenSet[str], float]:
+        """A mapping from pairs of adjacent nodes to the distance between them.
+        """
+        return dict(self._edges)
+    @property
+    def edges(self) -> Set[FrozenSet[str]]:
+        """A set of all pairs of adjacent nodes.
+        """
+        return {edge[0] for edge in self._edges}
+    @property
+    def heuristics(self) -> Dict[str, float]:
+        """A mapping from nodes to heuristics
+        """
+        return dict(self._heuristics)
+
+    def heuristic(self, node: str) -> float:
+        """Get the heuristic value for a node, or 0 if the node has no heuristic value.
+        """
+        return self.heuristics.get(node, 0)
+
+    def neighbors(self, node: str) -> Set[str]:
+        """Get a safe-to-modify set of e the neighbors of the given node.
+        """
+        return {n for edge in self.edges
+                for n in edge if node in edge} - {node}
+
+    def distance(self, a: str, b: str) -> float:
+        """Get the distance between two nodes.
+
+        If the nodes are not connected, KeyError.
+        """
+        return self.edge_lengths[frozenset({a, b})]
+
+    def path_cost(self, path: Path) -> float:
+        """Get the total distance of the edges connecting the listed nodes.
+
+        If any two adjacent nodes in the path are not connected, KeyError.
+        """
+        return sum(starmap(self.distance, zip(path, path[1:])))
 
     @classmethod
     def from_lines(cls, lines: Iterable[str]) -> 'Graph':
@@ -29,6 +95,7 @@ class Graph:
 
         section, line_regex = edges, edge_regex
         for line in lines:
+            # Allow comments and blank lines, for convenience
             if line.startswith("//") or re.fullmatch(r'\s*', line) is not None:
                 continue
             elif re.fullmatch(r'#####\s+', line) is not None:
@@ -57,18 +124,18 @@ class Graph:
             raise ValueError("Multiple edges between same nodes")
 
         # Convert for graph creation
-        edges = dict(map(lambda x: (frozenset(x[:2]), x[2]), edges))
-        heuristics = dict(heuristics)
+        edges = dict(map(lambda x: (frozenset(x[:2]), float(x[2])), edges))
+        heuristics = {node: float(h) for node, h in heuristics}
+
+        # Validate that node names are good
+        edge_nodes = {name for edge in edges for name in edge}
+        all_nodes = edge_nodes.union(heuristics.keys())
+        no_heuristic_nodes = edge_nodes.difference(heuristics.keys())
+        if len(no_heuristic_nodes) != 1:
+            raise ValueError("More than one node has no heuristic value")
+
         return cls(edges, heuristics)
 
-    def get_adjacent_states(self, state: str):
-        return [(set(key) - {state}).pop() for key in self.edges.keys() if state in key]
-
-    def get_heuristic(self, state1: str, state2: str):
-        return self.heuristics[frozenset({state1, state2})]
-
-    def get_weight(self, state1: str, state2: str):
-        return self.edges[frozenset({state1, state2})]
 
 class TreeNode:
     def __init__(self, graph: Graph, state: str, parent: 'TreeNode' = None):
@@ -89,7 +156,8 @@ class TreeNode:
         return self._print_path(self.trace_path())
 
     def expand(self) -> List['TreeNode']:
-        return list(self.__class__(self.graph, state, self) for state in self.graph.get_adjacent_states(self.state))
+        return list(self.__class__(self.graph, state, self) for state in self.graph.neighbors(self.state))
+
 
 class Problem(NamedTuple):
     graph: Graph
@@ -117,8 +185,9 @@ def General_Search(problem: Problem, search_method: SearchMethod):
     print(f'   failure to find path between {problem.initial_state} and {problem.solution_state}')
     return None
 
-def main(search_methods: Dict[str, SearchMethod]):
-    args = sys.argv[1:]
+
+def main(search_methods: Dict[str, SearchMethod], argv):
+    args = argv[1:]
     assert(len(args) == 1), f'Must have exactly 1 argument. Arguments detected: {args}'
 
     with open(args[0], 'r') as f:
@@ -135,7 +204,7 @@ def main(search_methods: Dict[str, SearchMethod]):
 SEARCH METHOD DEFINITIONS
 """
 search_methods: MutableMapping[str, SearchMethod] = OrderedDict()
-def register(name: str):
+def searchmethod(name: str):
     """Register a search method with a name
     """
     def decorator(function):
@@ -144,54 +213,62 @@ def register(name: str):
         return function
     return decorator
 
-@register('Breadth 1st search')
-def breadth_first_search(queue: List[TreeNode], new_nodes_list: List[TreeNode]) -> List[TreeNode]:
-    return queue + new_nodes_list
-
-@register('Depth 1st search')
+@searchmethod('Depth 1st search')
 def depth_first_search(queue: List[TreeNode], new_nodes_list: List[TreeNode]) -> List[TreeNode]:
-    return new_nodes_list + queue
+    return sorted(new_nodes_list, key=lambda treenode: treenode.state) + queue
 
 
-@register("Depth-limited search (depth limit 2)")
+@searchmethod('Breadth 1st search')
+def breadth_first_search(queue: List[TreeNode], new_nodes_list: List[TreeNode]) -> List[TreeNode]:
+    return queue + sorted(new_nodes_list, key=lambda treenode: treenode.state)
+
+
+@searchmethod("Depth-limited search (depth limit 2)")
 def depth_limited_2(open_nodes, new_nodes):
     return depth_limited(2, open_nodes, new_nodes)
 def depth_limited(n, open_nodes, new_nodes):
     print("   Not Implemented")
     ...
 
-@register("Iterative deepening search")
+
+@searchmethod("Iterative deepening search")
 def iterative_deepening(open_nodes, new_nodes):
     print("   Not Implemented")
     ...
 
-@register("Uniform cost search")
+
+@searchmethod("Uniform cost search")
 def uniform_cost(open_nodes, new_nodes):
     print("   Not Implemented")
     ...
 
-@register("Greedy search")
+
+@searchmethod("Greedy search")
 def greedy(open_nodes, new_nodes):
     print("   Not Implemented")
     ...
 
-@register("A*")
+
+@searchmethod("A*")
 def astar(open_nodes, new_nodes):
     print("   Not Implemented")
     ...
 
-@register("Hill-climbing search")
+
+@searchmethod("Hill-climbing search")
 def hill_climbing(open_nodes, new_nodes):
     print("   Not Implemented")
     ...
 
-@register("Beam search (w=2)")
+
+@searchmethod("Beam search (w=2)")
 def beam_2(open_nodes, new_nodes):
     return beam(2, open_nodes, new_nodes)
 def beam(n, open_nodes, new_nodes):
     print("   Not Implemented")
     ...
 
+
 # Will run at script execution
 if __name__ == '__main__':
-    main(search_methods)
+    main(search_methods, sys.argv)
